088a2e2ff1217a89ea756602e633516f1d89e14c
unknown <arestre@ncsu.edu>
2022-07-05 00:48:53 -0400

    working on portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,6 +3,7 @@ import BestFurrends from '../../assets/portfolio-mockups/best-furrends.JPG';
 import PasswordGenerator from '../../assets/portfolio-mockups/password-generator.JPG';
 import PickAFlic from '../../assets/portfolio-mockups/pick-a-flic.JPG';
 import WeatherDashboard from '../../assets/portfolio-mockups/weather-dashboard.JPG';
+import Github from '../../assets/github-icon.jpg';
 
 function Portfolio() {
 
@@ -28,13 +29,13 @@ function Portfolio() {
 
             <div className='portfolio'>
                 <div className='portfolio-mock'>
-                    <a href='https://karlyweatherstein.github.io/best-furrends/'>
-                        <img alt='best-furrends' src={BestFurrends}></img>
+                    <a href='https://arestrepo12.github.io/password-generator/'>
+                        <img alt='password-generator' src={PasswordGenerator}></img>
                     </a>
                 </div>
                 <div className='icon'>
-                    <h2>Best Furrends</h2>
-                    <a href='https://github.com/Karlyweatherstein/best-furrends'>
+                    <h2>Password Generator</h2>
+                    <a href='https://github.com/ARestrepo12/password-generator'>
                         <img src={Github} alt='Github'></img>
                     </a>
                     <p>Tools: HTML, CSS, Javascript</p>
