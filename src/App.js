95b0583b502b245bc680de4d4df0d90c89d1e14e
unknown <arestre@ncsu.edu>
2022-07-04 23:30:35 -0400

    working on routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import './App.css';
-import Nav from './components/Nav';
-import Hero from './components/Hero';
-import Footer from './components/Footer';
-import About from './components/About'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Resume from "./components/Resume";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Nav></Nav>
-      <Hero></Hero>
-      <About></About>
-      <Footer></Footer>
+      <Nav />
+      <Routes>
+        <Route path= "/" element={<Hero />} />
+        <Route path= "/resume" element={<Resume />} />
+        <Route path= "/portfolio" element={<Portfolio />} />
+        <Route path= "/about" element={<About />} />
+        <Route path= "/contact" element={<Contact />} />   
+      </Routes>
+      <Footer />
     </div>
+    </Router>
   );
 }
 
